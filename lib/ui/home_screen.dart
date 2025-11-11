9a7899917490d44c0fcd3252b4f0592b9b58e6a6
AdizAjah <rainadizd@gmail.com>
2025-11-11 09:34:42 +0700

    ui lagi

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:attendance_app/ui/absent/absent_screen.dart';
+import 'package:attendance_app/ui/attend/attend_screen.dart';
+import 'package:attendance_app/ui/attendance_history/attendance_history.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,16 +11,144 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          // Header
           SizedBox(
-            height: 50, // Header
-            child: Placeholder(),
+            height: 50,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.blueAccent,
+                centerTitle: true,
+                title: const Text(
+                  "Attendance - Flutter App Admin",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ),
+
+          // Content
           Expanded(
-            child: Placeholder(), // Content
+            child: Column(
+              children: [
+                Expanded(
+                  // efect when click
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AttendScreen(),
+                        ),
+                      );
+                    },
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/ic_absen.png'),
+                          height: 100,
+                          width: 100,
+                        ),
+                        Text(
+                          "Attendance Record",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AbsentScreen(),
+                        ),
+                      );
+                    },
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/ic_leave.png'),
+                          height: 100,
+                          width: 100,
+                        ),
+                        Text(
+                          "Permission",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const AttendanceHistoryScreen(),
+                        ),
+                      );
+                    },
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/ic_history.png'),
+                          height: 100,
+                          width: 100,
+                        ),
+                        Text(
+                          "Attendance History",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+
+          // Footer
           SizedBox(
-            height: 50, // Footer
-            child: Placeholder(),
+            height: 50,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.blueAccent,
+                centerTitle: true,
+                title: const Text(
+                  "IDN Boarding School Solo",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
